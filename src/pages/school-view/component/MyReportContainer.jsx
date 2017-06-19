0660a16f2f2b57ecb1d9b8dae419fd6662c44e9e
gaoshuai <gao9897@126.com>
2017-06-19 16:51:01 +0800

    加入加载页面

--- src/pages/school-view/component/MyReportContainer.jsx
+++ src/pages/school-view/component/MyReportContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MyReportList from './MyReportList';
 import $ from 'jquery';
+import Preloader from './Preloader';
 
 import getCookie from './../../../lib/getCookie';
 import PropTypes from 'prop-types';
@@ -11,7 +12,8 @@ class MyReport extends React.Component {
     constructor(){
         super();
         this.state={
-            myreport:null
+            myreport:null,
+            classPreloader:''
         }
     }
 
@@ -23,7 +25,8 @@ class MyReport extends React.Component {
         let url = config.API_DOMAIN + config.API_GET_REPOET_LIST;
         $.post(url ,data,function (response) {
             this.setState({
-                myreport:response
+                myreport:response,
+                classPreloader:'loaded'
             })
         }.bind(this),'json')
     }
@@ -31,7 +34,11 @@ class MyReport extends React.Component {
     render() {
         console.log(this.state.myreport);
         return (
-            <MyReportList myreport = {this.state.myreport}  />
+            <div className={this.state.classPreloader}>
+                <Preloader />
+                <MyReportList myreport = {this.state.myreport}  />
+            </div>
+
         )
     }
 }
