236579f8e2c09724ac0e3a981ca5203ce0be6dd5
gaoshuai <gao9897@126.com>
2017-06-20 13:46:02 +0800

    更改配置文件

--- src/config/production.jsx
+++ src/config/production.jsx
@@ -1,40 +1,24 @@
 module.exports = {
     // School view
-    API_DOMAIN: 'http://www.k12ke.com',
+    API_DOMAIN: 'http://59.110.7.209:4500',
+
+    API_GET_TOKEN:'/oauth/token',
+    API_up_TOKEN:'/oauth/token',
+    API_GET_TODO_LIST:'/api/v1.2/online_tests/todo_list',
+    API_GET_TESTED_LIST:'/api/v1.2/online_tests/tested_list',
+    API_GET_REPOET_LIST:'/api/v1.2/reports/my_list',
+    API_SUBMIT_RESULT:'/api/v1.2/online_tests/zh_result',
+
+    // Report URL
+    URL_MQ_REPORT_QUESTION: '/zx-survey/apps/report-mq/report-question',
+    URL_MQ_REPORT_VIEW: '/zx-survey/apps/report-mq/report-view',
+
+    //Report ID
+    SURVEY_MQ_QUESTION: '5939ffa94dd5e005ce0167b6',
+    SURVEY_PQ_QUESTION: 'dsfdfdfsdfdsfdsf',
+
     API_GET_BINDED_USERS: '/wx/auths/get_binded_users',
     API_CHECK_BIND: '/wx/auths/check_bind',
     API_BIND_USER: '/wx/bind',
-    API_UNBIND_USER: '/wx/auths/unbind',
-    API_GET_REPORT_LIST_ACADEMIC: '/api/wx/v1.1/reports/list',
-    API_GET_REPORT_LIST_OTHER: '/api/wx/v1.1/reports/list2',
-    API_GET_REPORT_DETAILS: '/wx/reports/get_pupil_report',
-    API_GRADE_KLASS_LIST: '/api/wx/v1.1/tenants/grade_klass_list',
-    API_GRADE_PUPIL_LIST: '/api/wx/v1.1/tenants/klass_pupil_list',
-    API_QUIZS_DETAILS: '/api/wx/v1.1/quizs/detail',
-    API_KLASS_LIST: '/api/wx/v1.1/reports/klass_list',
-
-    // Report URL
-    URL_REPORT_ACADEMIC_STUDENT: '/apps/report-page/academic/student',
-    URL_REPORT_ACADEMIC_CLASS: '/apps/report-page/academic/class',
-    URL_REPORT_ACADEMIC_GRADE: '/apps/report-page/academic/grade',
-    URL_REPORT_OTHER_STUDENT: '/apps/report-page/others/multiIntelligence',
-
-    API_GET_REPORT_PUPIL: '/api/wx/v1.1/reports/pupil',
-    API_GET_REPORT_KLASS: '/api/wx/v1.1/reports/klass',
-    API_GET_REPORT_GRADE: '/api/wx/v1.1/reports/grade',
-    API_GET_REPORT_PROJECT: '/api/wx/v1.1/reports/project',
-
-    // Reference
-    REFERENCE_PROJECT: 'project',
-    REFERENCE_PROJECT_LABEL: '整体',
-    REFERENCE_GRADE: 'grade',
-    REFERENCE_GRADE_LABEL: '年级',
-    REFERENCE_CLASS: 'class',
-    REFERENCE_CLASS_LABEL: '班级',
-
-    // Report type
-    REPORT_TYPE_PUPIL: 'pupil',
-    REPORT_TYPE_KLASS: 'klass',
-    REPORT_TYPE_GRADE: 'grade',
-    REPORT_TYPE_PROJECT: 'project'
+    API_UNBIND_USER: '/wx/auths/unbind'
 };
